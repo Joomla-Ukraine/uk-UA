4a04edb43dc84924465ebe2124b5abd5c94a737f
Den <dgm.denys@gmail.com>
2020-05-07 08:51:11 +0300

    Додано строкові суфікси

--- uk-UA_3.x/language/uk-UA/uk-UA.localise.php
+++ uk-UA_3.x/language/uk-UA/uk-UA.localise.php
@@ -30,7 +30,13 @@ abstract class Uk_UALocalise
 			return [ '0' ];
 		}
 
-		return [ $count % 10 == 1 && $count % 100 != 11 ? '1' : $count % 10 >= 2 && $count % 10 <= 4 && ($count % 100 < 10 || $count % 100 >= 20) ? '2' : 'MORE' ];
+		return $count % 10 == 1 && $count % 100 != 11 ? [
+			'ONE',
+			'1'
+		] : $count % 10 >= 2 && $count % 10 <= 4 && ($count % 100 < 10 || $count % 100 >= 20) ? [ '2' ] : [
+			'OTHER',
+			'MORE'
+		];
 	}
 
 	/**
