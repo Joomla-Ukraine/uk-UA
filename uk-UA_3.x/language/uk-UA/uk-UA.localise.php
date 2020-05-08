1b95508291f4de2b20de489a30a07d5dbd6356ac
Dmitry Sakharuk <saharin.y@gmail.com>
2020-05-08 14:11:18 +0300

    Fix Unparenthesized `a ? b : c ? d : e` is deprecated

--- uk-UA_3.x/language/uk-UA/uk-UA.localise.php
+++ uk-UA_3.x/language/uk-UA/uk-UA.localise.php
@@ -30,7 +30,7 @@ abstract class Uk_UALocalise
 			return [ '0' ];
 		}
 
-		return [ $count % 10 == 1 && $count % 100 != 11 ? '1' : $count % 10 >= 2 && $count % 10 <= 4 && ($count % 100 < 10 || $count % 100 >= 20) ? '2' : 'MORE' ];
+		return [ ($count % 10 == 1 && $count % 100 != 11 ? '1' : ($count % 10 >= 2 && $count % 10 <= 4 && ($count % 100 < 10 || $count % 100 >= 20) ? '2' : 'MORE')) ];
 	}
 
 	/**
